800f029a4c844d5b1810e611cf221687a0c4a717
Aman Sagar <amansagar887@gmail.com>
2022-12-14 17:53:46 +0530

    third

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,22 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
-        super(false);
+        super(isVeg);
         // your code goes here
+        addExtraCheese();
+        addExtraToppings();
+    }
+    @Override
+    public void addExtraToppings(){
+        super.addExtraToppings();
+    }
+
+    @Override
+    public void addTakeaway() {
+        super.addTakeaway();
+    }
+    @Override
+    public String getBill(){
+        return super.getBill();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,6 +5,10 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    boolean isCheesadded = false;
+    boolean isToppingadded = false;
+    boolean isbilljenerated = false;
+    boolean isbagadded = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
@@ -24,27 +28,35 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(isCheesadded) return;
         this.price+=80;
+        isCheesadded = true;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isToppingadded) return;
         if(isVeg){
             this.price+=70;
         }
         else{
             this.price+=120;
         }
+        isToppingadded = true;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(isbagadded) return;
         this.price+=20;
+        isbagadded = true;
     }
 
     public String getBill(){
         // your code goes here
-        this.bill = String.valueOf(price);
+        if(isbilljenerated==false)this.bill = String.valueOf(price);
+        isbilljenerated = true;
         return this.bill;
+
     }
 }
